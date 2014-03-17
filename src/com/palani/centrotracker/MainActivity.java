de69b695400a8be7f6102609dbb43030ff13d3b7
pmuthukumarasamy <palandrodev@gmail.com>
2014-03-17 14:08:43 -0400

    Database added

--- src/com/palani/centrotracker/MainActivity.java
+++ src/com/palani/centrotracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.palani.centrotracker;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,6 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
@@ -33,6 +35,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.palani.centrotracker.database.DatabaseHelper;
+import com.palani.centrotracker.util.Utility;
 import com.palani.dataModel.Bus;
 
 public class MainActivity extends Activity {
@@ -45,13 +49,17 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		DatabaseHelper dbh = new DatabaseHelper(this);
+		LOGGER.log(Level.SEVERE,dbh.getWritableDatabase().getPath());
 		// Get a handle to the Map Fragment
 		final GoogleMap map = ((MapFragment) getFragmentManager()
 				.findFragmentById(R.id.map)).getMap();
 
+		
+		
 		map.setBuildingsEnabled(true);
 		map.setMyLocationEnabled(true);
-
+		
 		UiSettings uiSettings = map.getUiSettings();
 		uiSettings.setMyLocationButtonEnabled(true);
 		uiSettings.setCompassEnabled(true);
@@ -366,7 +374,7 @@ public class MainActivity extends Activity {
 				.anchor(0.0f, 1.0f).title("Centro Transit Hub")
 				.position(new LatLng(43.04320, -76.15116)));
 
-		/* Full Map */
+//		 Full Map 
 		PolylineOptions options = new PolylineOptions();
 		options.geodesic(true);
 		options.color(Color.BLUE);
@@ -732,13 +740,39 @@ public class MainActivity extends Activity {
 					.add(new LatLng(43.14944, -76.19347))
 					.add(new LatLng(43.14919, -76.19346))
 					.add(new LatLng(43.14876, -76.19342))
-			/*
-			 * // Wetzel & Moltlage
-			 */;
+			
+			  // Wetzel & Moltlage
+			 ;
 		}
 
 		map.addPolyline(options);
 
+		dbh.getWritableDatabase().beginTransaction();
+		try{
+			String sql = "INSERT INTO ROUTE (id,routenumber,direction,map) values (1,286,'From Downtown To HenryClay','"+Utility.encode(options.getPoints())+"');";
+			
+			String select = "SELECT * FROM ROUTE;";
+			
+			
+			
+			LOGGER.log(Level.SEVERE,"TRYING "+sql);
+			
+			dbh.getWritableDatabase().execSQL(sql);
+			
+			Cursor cursor = dbh.getReadableDatabase().rawQuery(select, null);
+			
+			cursor.moveToFirst();
+			do{
+				LOGGER.log(Level.SEVERE,Arrays.toString(cursor.getColumnNames())+"Columns");
+			}while(cursor.moveToNext());
+			LOGGER.log(Level.SEVERE,cursor.getCount()+"results");
+			
+			dbh.getWritableDatabase().endTransaction();
+		dbh.close();
+		}catch(Exception ex){
+			LOGGER.log(Level.SEVERE,ex.getMessage());
+		}
+		
 	}
 
 	@Override
@@ -749,77 +783,6 @@ public class MainActivity extends Activity {
 	}
 	
 	
-	/**
-     * Decodes an encoded path string into a sequence of LatLngs.
-     */
-    public static List<LatLng> decode(final String encodedPath) {
-        int len = encodedPath.length();
 
-        // For speed we preallocate to an upper bound on the final length, then
-        // truncate the array before returning.
-        final List<LatLng> path = new ArrayList<LatLng>();
-        int index = 0;
-        int lat = 0;
-        int lng = 0;
-
-        while (index < len) {
-            int result = 1;
-            int shift = 0;
-            int b;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lat += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            result = 1;
-            shift = 0;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lng += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            path.add(new LatLng(lat * 1e-5, lng * 1e-5));
-        }
-
-        return path;
-    }
-
-    /**
-     * Encodes a sequence of LatLngs into an encoded path string.
-     */
-    public static String encode(final List<LatLng> path) {
-        long lastLat = 0;
-        long lastLng = 0;
-
-        final StringBuffer result = new StringBuffer();
-
-        for (final LatLng point : path) {
-            long lat = Math.round(point.latitude * 1e5);
-            long lng = Math.round(point.longitude * 1e5);
-
-            long dLat = lat - lastLat;
-            long dLng = lng - lastLng;
-
-            encode(dLat, result);
-            encode(dLng, result);
-
-            lastLat = lat;
-            lastLng = lng;
-        }
-        return result.toString();
-    }
-
-    private static void encode(long v, StringBuffer result) {
-        v = v < 0 ? ~(v << 1) : v << 1;
-        while (v >= 0x20) {
-            result.append(Character.toChars((int) ((0x20 | (v & 0x1f)) + 63)));
-            v >>= 5;
-        }
-        result.append(Character.toChars((int) (v + 63)));
-    }
 
 }
